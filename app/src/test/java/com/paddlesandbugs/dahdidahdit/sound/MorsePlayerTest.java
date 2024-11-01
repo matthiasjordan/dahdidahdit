0ee150caa62842928cc550bb1f5fe9c4b9c0f98e
Matthias Jordan <mjordan@code-fu.de>
2024-11-01 09:58:33 +0100

    Added some license headers

--- app/src/test/java/com/paddlesandbugs/dahdidahdit/sound/MorsePlayerTest.java
+++ app/src/test/java/com/paddlesandbugs/dahdidahdit/sound/MorsePlayerTest.java
@@ -1,3 +1,21 @@
+/****************************************************************************
+    Dahdidahdit - an Android Morse trainer
+    Copyright (C) 2021-2024 Matthias Jordan <matthias@paddlesandbugs.com>
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ ****************************************************************************/
+
 package com.paddlesandbugs.dahdidahdit.sound;
 
 import org.junit.Assert;
