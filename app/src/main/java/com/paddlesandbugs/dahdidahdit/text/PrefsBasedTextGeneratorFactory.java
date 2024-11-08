56c2fb5abcd2e31a8001b318fcd6fb7e947dc331
Matthias Jordan <mjordan@code-fu.de>
2024-11-08 23:14:19 +0100

    Issue 2: Sending training's random generator can honor Koch level

--- app/src/main/java/com/paddlesandbugs/dahdidahdit/text/PrefsBasedTextGeneratorFactory.java
+++ app/src/main/java/com/paddlesandbugs/dahdidahdit/text/PrefsBasedTextGeneratorFactory.java
@@ -1,26 +1,24 @@
 /****************************************************************************
-    Dahdidahdit - an Android Morse trainer
-    Copyright (C) 2021-2024 Matthias Jordan <matthias@paddlesandbugs.com>
+ Dahdidahdit - an Android Morse trainer
+ Copyright (C) 2021-2024 Matthias Jordan <matthias@paddlesandbugs.com>
 
-    This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
+ This program is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
 
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
 
-    You should have received a copy of the GNU General Public License
-    along with this program.  If not, see <https://www.gnu.org/licenses/>.
-****************************************************************************/
+ You should have received a copy of the GNU General Public License
+ along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ ****************************************************************************/
 
 package com.paddlesandbugs.dahdidahdit.text;
 
 import android.content.Context;
-
-import java.util.Set;
 
 import com.paddlesandbugs.dahdidahdit.Distribution;
 import com.paddlesandbugs.dahdidahdit.MorseCode;
@@ -29,10 +27,14 @@ import com.paddlesandbugs.dahdidahdit.copytrainer.CopyTrainerParamsFaded;
 import com.paddlesandbugs.dahdidahdit.copytrainer.TextGeneratorFactory;
 import com.paddlesandbugs.dahdidahdit.params.GeneralFadedParameters;
 
+import java.util.Set;
+
 /**
  * Generates {@link TextGenerator} instances based on the given type (taken from preferences).
  */
 public class PrefsBasedTextGeneratorFactory {
+
+    private static final Set<String> KOCH_LEVEL_HONORING_GENERATORS = Set.of("random");
 
     private final String receivedFileName;
 
@@ -49,11 +51,9 @@ public class PrefsBasedTextGeneratorFactory {
         this.kochLevel = kochLevel;
     }
 
-
     public void setWordListCount(int wordListCount) {
         this.wordListCount = wordListCount;
     }
-
 
     public TextGenerator getGenerator(Context context, String generatorType) {
         final TextGenerator tg;
@@ -84,8 +84,6 @@ public class PrefsBasedTextGeneratorFactory {
                 break;
             }
             default: {
-                final MorseCode.CharacterList characters = MorseCode.getInstance().getCharacters();
-                Set<MorseCode.CharacterData> set = MorseCode.asSet(characters);
                 TextGeneratorFactory.DistributionFunction df = new TextGeneratorFactory.DistributionFunction() {
                     @Override
                     public Distribution<MorseCode.CharacterData> applyWeights(Distribution<MorseCode.CharacterData> dist) {
@@ -99,6 +97,16 @@ public class PrefsBasedTextGeneratorFactory {
             }
         }
         return tg;
+    }
+
+    /**
+     * Checks if the generator whose prefs name is given honors the setting that only characters up to the given Koch level are to be used.
+     *
+     * @param generatorName the prefs name of the generator
+     * @return true, if the generator can restrict its output to the given Koch level. Else false.
+     */
+    public static boolean isHonoringKochLevel(String generatorName) {
+        return KOCH_LEVEL_HONORING_GENERATORS.contains(generatorName);
     }
 
 
